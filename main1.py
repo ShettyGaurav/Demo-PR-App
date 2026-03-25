bd1309cae110676f8c46481b5e5cd44fa3ac5168
Shetty Gaurav Jagadeesha Shetty <sheshett@sheshett-thinkpadp1gen4i.bengluru.csb>
2026-03-25 10:23:06 +0530

    Added main1.py

---
+++ main1.py
@@ -0,0 +1,9 @@
+# Get user input
+name = input("What is your name? ")
+age = int(input("How old are you? ")) # We convert the input string to an integer
+
+# Perform a simple calculation
+next_year_age = age + 1
+
+# Output the result using an f-string (formatted string)
+print(f"Hi {name}! Next year, you will be {next_year_age} years old.")s
